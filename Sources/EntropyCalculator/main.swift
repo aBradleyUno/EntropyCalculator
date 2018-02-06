e33d003560518ef8a5024fa816df4e9d3b152724
Alexander Bradley <alexandro20.0@icloud.com>
2018-02-06 11:14:26 -0600

    Funcionalidad agregada, no se hacen checks

--- Sources/EntropyCalculator/main.swift
+++ Sources/EntropyCalculator/main.swift
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import Foundation
+
+print("Cuantos estados?")
+if let nEstados = Int(readLine()!) {
+    var estados: [Float] = [Float]()
+    for i in 1...nEstados {
+        print("P(\(i))?")
+        estados.append(Float(readLine()!)!)
+    }
+    var resultado: Float = 0
+    for estado in estados {
+        resultado += estado * log2(estado)
+    }
+    print(resultado * -1)
+}
